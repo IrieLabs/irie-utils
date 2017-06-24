a8a829fda7ad4238a9e5caff23bb3bef1dc7c6ff
Gaston <gaston.elhordoy@gmail.com>
2017-06-24 20:10:36 -0300

    feat(fs): add utility method to write a readable stream to a temp file

--- libraries/fs.js
+++ libraries/fs.js
@@ -6,6 +6,7 @@ const tmp = require('./tmp')
 module.exports = {
   readHugeFile,
   tmpFile: tmp.file,
+  tmpFileFromStream: tmp.fileFromStream,
   countFileLines: (filePath, discardEmpties) => {
     return readHugeFile(filePath, undefined, { discardEmpties })
   }
--- libraries/tmp.js
+++ libraries/tmp.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const _ = require('lodash')
+const Promise = require('bluebird')
+const fs = require('fs')
 
 const promisificationOptions = {
   multiArgs: true,
@@ -8,18 +10,35 @@ const promisificationOptions = {
     return !_.endsWith(name, 'Sync')
   }
 }
-
-const Promise = require('bluebird')
 const tmp = Promise.promisifyAll(require('tmp'), promisificationOptions)
 
+function createTempFile (extension, prefix) {
+  return tmp.fileAsync({
+    prefix: prefix || 'tmp-',
+    postfix: '.' + (extension || 'tmp')
+  })
+  .spread((path, fd, cleaner) => {
+    return { path, fd, cleaner }
+  })
+}
+
+function createTempFileFromStream (inputStream, extension, prefix) {
+  return createTempFile()
+  .then(tmpFile => {
+    return new Promise((resolve, reject) => {
+      const outputStream = fs.createWriteStream(tmpFile.path).on('error', reject).on('finish', resolve)
+      inputStream.pipe(outputStream)
+    })
+    .then(() => tmpFile)
+    .catch(err => {
+      // remove temp file upon error
+      tmpFile.cleaner()
+      return Promise.reject(err)
+    })
+  })
+}
+
 module.exports = {
-  file: function createTempFile (extension, prefix) {
-    return tmp.fileAsync({
-      prefix: prefix || 'tmp-',
-      postfix: '.' + (extension || 'tmp')
-    })
-    .spread(function (path, fd, cleaner) {
-      return { path, fd, cleaner }
-    })
-  }
+  file: createTempFile,
+  fileFromStream: createTempFileFromStream
 }
