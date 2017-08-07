f415e04e5d8755a639d22ef1a91e5d60f32a8c4d
Gaston <gaston.elhordoy@gmail.com>
2017-08-07 16:27:01 +0800

    feat(strings): add isJSON functionality

--- libraries/strings.js
+++ libraries/strings.js
@@ -3,10 +3,12 @@
 const noDiacritics = require('diacritics').remove
 const uuid = require('uuid/v4')
 const shortid = require('shortid').generate
+const isJSON = require('is-json')
 
 module.exports = {
   uuid,
   noDiacritics,
   shortid,
-  uniqueid: shortid
+  uniqueid: shortid,
+  isJSON
 }
