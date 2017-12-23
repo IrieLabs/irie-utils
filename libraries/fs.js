eb95ebf36cdeb070cb3718a9f172753de3cf8594
Gaston <gaston.elhordoy@gmail.com>
2017-12-22 23:12:22 -0300

    feat: add utility for sanitizing names for fs objects

--- libraries/fs.js
+++ libraries/fs.js
@@ -2,11 +2,13 @@
 
 const readHugeFile = require('./read-huge-file')
 const tmp = require('./tmp')
+const sanitize = require('sanitize-filename')
 
 module.exports = {
   readHugeFile,
   tmpFile: tmp.file,
   tmpFileFromStream: tmp.fileFromStream,
+  sanitize,
   countFileLines: (filePath, discardEmpties) => {
     return readHugeFile(filePath, undefined, { discardEmpties })
   }
