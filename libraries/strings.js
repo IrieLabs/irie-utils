acf67b02aab76918cbf07db4a76cf2caec6640a5
Gaston Elhordoy <gaston.elhordoy@gmail.com>
2020-09-11 19:19:43 -0300

    fix: check for root arrays in the isJSON utility

--- libraries/strings.js
+++ libraries/strings.js
@@ -5,10 +5,26 @@ const uuid = require('uuid/v4')
 const shortid = require('shortid').generate
 const isJSON = require('is-json')
 
+const ARRAY_PATTERN = /^\[.*\]$/
+
+function isJSONWrapper (value) {
+  if (isJSON(value)) {
+    return true
+  } else if (ARRAY_PATTERN.test(value)) {
+    try {
+      value = JSON.parse(value)
+      return Array.isArray(value)
+    } catch (err) {
+      // fall through
+    }
+  }
+  return false
+}
+
 module.exports = {
   uuid,
   noDiacritics,
   shortid,
   uniqueid: shortid,
-  isJSON
+  isJSON: isJSONWrapper
 }
